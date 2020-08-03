352f84cedfd0fc5e4ca9a41bca8e92f4cd5712cd
Peter-Roh <rmc2@naver.com>
2020-08-03 20:41:12 +0900

    add mypage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Login from './Routes/Login';
 import Main from './Routes/Main';
+import Mypage from './Routes/Mypage';
 import logo from './logo.svg';
 import './App.css';
 import { firestore } from "./firebase";
@@ -11,6 +12,7 @@ function App() {
     <HashRouter>
       <Route path="/" exact={true} component={Login} />
       <Route path="/main" exact={true} component={Main} />
+      <Route path="/mypage" exact={true} component={Mypage} />
     </HashRouter>
   );
 }
