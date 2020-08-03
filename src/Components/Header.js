1d2b0ebbb68241b9094b8260ab95f676bf5f38ec
Peter-Roh <rmc2@naver.com>
2020-08-03 17:01:47 +0900

    adding router, login, main page

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+            <div className="header__box">
+                <div>image</div>
+                <div>Pocket Market</div>
+                <div>my</div>
+            </div>
+        );
+}
+
+export default Header;
---
+++ src/Routes/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+function Main() {
+    return (
+            <>
+                <Header />
+                <div className="main">
+                    <div className="main__navigation">
+                        <div className="navigation__festival">축제보기</div>
+                        <div className="navigation__store">매장보기</div>
+                    </div>
+                    <div className="current-position">내 현재 위치 icon</div>
+                </div>
+            </>
+        );
+}
+
+export default Main;
