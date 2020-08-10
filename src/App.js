88f00cfdb5ee5da078a26f39c1dace784a897239
Peter-Roh <rmc2@naver.com>
2020-08-10 15:55:49 +0900

    order, order option, order info page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Routes/Login';
 import Main from './Routes/Main';
 import Mypage from './Routes/Mypage';
 import FestivalStore from './Routes/FestivalStore';
 import StoreView from './Routes/Store';
 import ReviewList from './Routes/ReviewList';
+import Order from './Routes/Order';
+import OrderInfo from './Routes/OrderInfo';
 import Fastorder from './Routes/Fastorder';
 import OrderHistory from './Routes/OrderHistory';
 import PointHistory from './Routes/PointHistory';
@@ -23,7 +25,11 @@ function App() {
             <Route exact path="/" component={Login} />
             <Route exact path="/main" component={Main} />
             <Route exact path="/main/festival/:id" component={FestivalStore} />
-            <Route exact path="/main/store/:storeId/:reviewId" component={ReviewList} />
+            <Switch>
+                <Route exact path="/main/store/:storeId/order" component={Order} />
+                <Route exact path="/main/store/:storeId/orderinfo" component={OrderInfo} />
+                <Route exact path="/main/store/:storeId/:reviewId" component={ReviewList} />
+            </Switch>
             <Route exact path="/main/store/:storeId" component={StoreView} />
             <Route exact path="/order/review" component={ReviewWrite} />
             <Route exact path="/mypage" component={Mypage} />
