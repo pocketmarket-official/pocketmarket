ee8faa465422192e62831c972e79422a3b7ae8be
shson <shson@shsonui-MacBookPro.local>
2020-08-11 23:26:57 +0900

    added kakao authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Routes/Login';
 import Main from './Routes/Main';
@@ -15,31 +15,36 @@ import PointHistory from './Routes/PointHistory';
 import QuestionsHistory from './Routes/QuestionsHistory';
 import QuestionWrite from './Routes/QuestionWrite';
 import ReviewWrite from './Routes/ReviewWrite';
-import logo from './logo.svg';
 import './App.css';
 import { firestore } from "./firebase";
+import AuthRoute from "./Security/AuthRoute";
 
 
 function App() {
+    const [user, setUser] = useState(null);
+    const authenticated = user != null;
+    const setUserInfo = ({ email }) => setUser({ email });
     return (
         <BrowserRouter>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/main" component={Main} />
+            <Route exact path="/login" render={props => (
+                <Login authenticated={authenticated} login={setUserInfo} {...props} />
+            )} />
+            <Route exact path="/" component={Main} />
             <Route exact path="/main/festival/:id" component={FestivalStore} />
             <Switch>
                 <Route exact path="/main/store/:storeId/order" component={Order} />
                 <Route exact path="/main/store/:storeId/orderinfo" component={OrderInfo} />
                 <Route exact path="/main/store/:storeId/:reviewId" component={ReviewList} />
             </Switch>
-            <Route exact path="/main/store/:storeId" component={StoreView} />
-            <Route exact path="/order/review" component={ReviewWrite} />
-            <Route exact path="/order/complete" component={OrderComplete} />
-            <Route exact path="/mypage" component={Mypage} />
-            <Route exact path="/mypage/fastorder" component={Fastorder} />
-            <Route exact path="/mypage/order" component={OrderHistory} />
-            <Route exact path="/mypage/point" component={PointHistory} />
-            <Route exact path="/mypage/questions" component={QuestionsHistory} />
-            <Route exact path="/mypage/questions/write" component={QuestionWrite} />
+            <AuthRoute authenticated={authenticated} exact path="/main/store/:storeId" component={StoreView} />
+            <AuthRoute authenticated={authenticated} exact path="/order/review" component={ReviewWrite} />
+            <AuthRoute authenticated={authenticated} exact path="/order/complete" component={OrderComplete} />
+            <AuthRoute authenticated={authenticated} exact path="/mypage" component={Mypage} />
+            <AuthRoute authenticated={authenticated} exact path="/mypage/fastorder" component={Fastorder} />
+            <AuthRoute authenticated={authenticated} exact path="/mypage/order" component={OrderHistory} />
+            <AuthRoute authenticated={authenticated} exact path="/mypage/point" component={PointHistory} />
+            <AuthRoute authenticated={authenticated} exact path="/mypage/questions" component={QuestionsHistory} />
+            <AuthRoute authenticated={authenticated} exact path="/mypage/questions/write" component={QuestionWrite} />
         </BrowserRouter>
     );
 }
