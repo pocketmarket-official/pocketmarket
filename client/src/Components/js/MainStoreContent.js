8b21838f538026f0ff6ae8e0d5bb66849b8face8
Peter-Roh <rmc2@naver.com>
2020-09-26 17:03:23 +0900

    fixing main_store

---
+++ client/src/Components/js/MainStoreContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import StoreJSX from './mainStoreJSX';
+
+
+function MainStoreContent(props) {
+    return (
+        <StoreJSX place={props} />
+    );
+}
+
+export default MainStoreContent;
