4d72d7679a9790501d5824ad52e4643e550d7fad
90majh <90majh@gmail.com>
2020-09-30 00:00:08 +0900

    tmp commit for modyfing store model

--- django-backend/stores/models.py
+++ django-backend/stores/models.py
@@ -2,23 +2,23 @@
 define stores related models
 """
 from django.db import models
+from datetime import datetime
 
+from django.apps import apps
 
 class Store(models.Model):
 
     """ Store Model Definition """
-"""
-    brandCd = models.Foreignkey(brand)
-    storeCd = models.CharField(max_length=10)
-    storeName = models.CharField(max_length=50)
-    storeCeo = models.CharField(max_length=50)
-    bizopNo = models.CharField(max_length=20)
-    storeTel = models.CharField(max_length=20)
-    storeMobile = models.CharField(max_length=20)
-    storeFax = models.CharField(max_length=20)
-    postCd = models.CharField(max_length=6)
-    storeMail = models.CharField(max_length=50)
-    storeAddr1 = models.CharField(max_length=200)
-    storeAddr2 = models.CharField(max_length=200)
-    storeOldAddr = models.CharField(max_length=200)
-"""
+    brandCd = models.ForeignKey('brands.Brand', on_delete=models.CASCADE, default=0)
+    storeCd = models.CharField(max_length=10, default='00000')
+    storeName = models.CharField(max_length=50, default='')
+    storeCeo = models.CharField(max_length=50, blank=True)
+    bizopNo = models.CharField(max_length=20, blank=True)
+    storeTel = models.CharField(max_length=20, blank=True)
+    storeMobile = models.CharField(max_length=20, blank=True)
+    storeFax = models.CharField(max_length=20, blank=True)
+    postCd = models.CharField(max_length=6, blank=True)
+    storeMail = models.CharField(max_length=50, blank=True)
+    storeAddr1 = models.CharField(max_length=200, blank=True)
+    storeAddr2 = models.CharField(max_length=200, blank=True)
+    storeOldAddr = models.CharField(max_length=200, blank=True)
