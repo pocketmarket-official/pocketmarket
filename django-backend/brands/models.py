eb7540cc36a6eeda64d7b3deb8d589971c958dbd
90majh <90majh@gmail.com>
2020-09-29 23:48:08 +0900

    modify brands model

--- django-backend/brands/models.py
+++ django-backend/brands/models.py
@@ -2,18 +2,16 @@
 define festivals related models
 """
 from django.db import models
+from datetime import datetime
 
 
 class Brand(models.Model):
 
     """ Brand Model Definition """
-"""
-    brandCd = models.CharField(max_length=5)
-    brandName = models.CharField(max_length=100)
-    useYn = models.CharField(max_length=1)
-    insDt = models.DateTimeField()
-    insUs = models.CharField(max_length=20)
-    modDt = models.DateTimeField()
-    modUs = models.CharField(max_lenght=20)
-
-"""
+    brandCd = models.CharField(max_length=5, default='00000')
+    brandName = models.CharField(max_length=100, default='')
+    useYn = models.CharField(max_length=1, default='Y')
+    insDt = models.DateTimeField(default=datetime.now())
+    insUs = models.CharField(max_length=30, default='defaultValue')
+    modDt = models.DateTimeField(default=datetime.now())
+    modUs = models.CharField(max_length=30, default='defaultValue')
