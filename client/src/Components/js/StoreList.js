ebed84f7f6b352afda27ac740792f51b77f0d0e4
Peter-Roh <rmc2@naver.com>
2020-09-30 16:02:24 +0900

    change store_list to class component

--- client/src/Components/js/StoreList.js
+++ client/src/Components/js/StoreList.js
@@ -2,28 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-let temp = [
-    {
-        id: 1,
-        username: "노민철",
-        like_count: 14,
-        order_date: "2020-09-10",
-        order_list: ["아이스아메리카노 2", "아이스라떼 1"],
-        comment: "커피가 정말 맛있어요",
-    },
-    {
-        id: 2,
-        username: "마진형",
-        like_count: 22,
-        order_date: "2020-09-11",
-        order_list: ["아이스아메리카노 1", "티라미수 1"],
-        comment: "맛있는 티라미수",
-    },
-]
-
-function StoreList() {
+function StoreList(props) {
     return(
-        temp.map((data) => {
+        props.data.map((data) => {
             return (
                 <>
                     <div className="review__container">
@@ -32,7 +13,7 @@ function StoreList() {
                             <div>image</div>
                             <div className="review__title">
                                 <div className="review__name">{data.username}</div>
-                                <div className="review__detail">리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰</div>
+                                <div className="review__detail">{data.review}</div>
                             </div>
                         </div>
                         </Link>
@@ -42,10 +23,11 @@ function StoreList() {
                                 <div className="review__order">{data.order_list}</div>
                                 <div className="review__likes">{data.like_count}</div>
                             </div>
-                            <div className="review__comment">댓글댓글</div>
-                            <div className="review__comment">댓글댓글</div>
-                            <div className="review__comment">댓글댓글</div>
-                            <div className="review__comment">댓글댓글</div>
+                            {data.comments.map((content) => {
+                                return (
+                                    <div>{content}</div>
+                                );
+                            })}
                         </div>
                     </div>
                 </>
--- client/src/Routes/Store.js
+++ client/src/Routes/Store.js
@@ -1,90 +1,118 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import HeaderBack from '../Components/js/HeaderBack';
 import StoreList from '../Components/js/StoreList';
 
 
-// react hooks that changes tabs
-const useTabs = (initialTab, allTabs) => {
-    const [currentIndex, setCurrentIndex] = useState(initialTab);
+class StoreView extends React.Component {
+    constructor(props) {
+        super(props);
 
-    if(!allTabs || !Array.isArray(allTabs)) {
-        return;
+        this.temp = [
+            {
+                id: 1,
+                username: "노민철",
+                like_count: 14,
+                order_date: "2020-09-10",
+                order_list: ["아이스아메리카노 2", "아이스라떼 1"],
+                comment: "커피가 정말 맛있어요",
+                review: '한입 와앙! 물고 놨는데 한도 끝도 없이 끊어지지 않고 늘어나는 치즈!',
+                storeName: '강남핫도그',
+                comments: ["감사합니다 길동님!"],
+            },
+            {
+                id: 2,
+                username: "마진형",
+                like_count: 22,
+                order_date: "2020-09-11",
+                order_list: ["아이스아메리카노 1", "티라미수 1"],
+                comment: "맛있는 티라미수",
+                review: '맵기만 한 떢볶이는 이제 그만! 화끈한 매운맛과 불맛, 그리고 건강까지...',
+                storeName: '조폭 떡볶이',
+                comments: ["감사합니다 길동님!", "가나다라마바사"],
+            },
+        ]
+
+        this.state = {
+            current: 0,
+            temp: this.temp,
+        }
+
+        this.handlePageGrid = this.handlePageGrid.bind(this);
+        this.handlePageList = this.handlePageList.bind(this);
+        this.handlePageRender = this.handlePageRender.bind(this);
+
+        const id = this.props.location.state.data.id;
+        const link = "/main/store/" + id + "/order";
     }
 
-    return {
-        currentItem: allTabs[currentIndex],
-        changeItem: setCurrentIndex
-    };
-};
+    handlePageGrid() {
+        this.setState(() => ({ current: 0 }));
+    }
 
-const content = [
-    {
-        tab: "grid", 
-        content:
-        <>
-            <div className="store__review grid" id="review__container">
-                <Link to="/review"><div>image1</div></Link>
-                <div>image2</div>
-                <div>image3</div>
-                <div>image1</div>
-                <div>image2</div>
-                <div>image3</div>
-                <div>image1</div>
-                <div>image2</div>
-                <div>image3</div>
-                <div>image1</div>
-                <div>image2</div>
-                <div>image3</div>
-                <div>image1</div>
-                <div>image2</div>
-            </div>
-        </>
-    }, 
-    {
-        tab: "list", 
-        content:
-        <>
-            <StoreList />
-        </>
-    },
-];
+    handlePageList() {
+        this.setState(() => ({ current: 1 }));
+    }
 
-function StoreView(props) {
-    const { currentItem, changeItem } = useTabs(0, content);
+    handlePageRender() {
+        if(this.state.current === 0) {
+            return (
+            <>
+                <div className="store__review grid" id="review__container">
+                    <Link to="/review"><div>image1</div></Link>
+                    <div>image2</div>
+                    <div>image3</div>
+                    <div>image1</div>
+                    <div>image2</div>
+                    <div>image3</div>
+                    <div>image1</div>
+                    <div>image2</div>
+                    <div>image3</div>
+                    <div>image1</div>
+                    <div>image2</div>
+                    <div>image3</div>
+                    <div>image1</div>
+                    <div>image2</div>
+                </div>
+            </>
+            );
+        } else if(this.state.current === 1) {
+            return <StoreList data={this.state.temp} />;
+        } else {
+            return null;
+        }
+    }
 
-    const id = props.location.state.data.id;
-    const link = "/main/store/" + id + "/order";
-
-    return (
-        <>
-            <HeaderBack url='/mypage' />
-            <div className="store">
-                <div className="content__store">
-                    <div className="store__store">
-                        <div className="store__image">image</div>
-                        <div className="store__detail">
-                            <div className="detail__title">
-                                <div className="detail__name">{props.location.state.data.store_nm}</div>
-                                <div className="detail__distance">{props.location.state.d}</div>
-                                <div className="detail__likes">{props.location.state.data.like_count}</div>
-                            </div>
-                            <div className="detail__description">
-                                {props.location.state.data.comment}
+    render() {
+        return (
+            <>
+                <HeaderBack url='/mypage' />
+                <div className="store">
+                    <div className="content__store">
+                        <div className="store__store">
+                            <div className="store__image">image</div>
+                            <div className="store__detail">
+                                <div className="detail__title">
+                                    <div className="detail__name">{this.props.location.state.data.store_nm}</div>
+                                    <div className="detail__distance">{this.props.location.state.d}</div>
+                                    <div className="detail__likes">{this.props.location.state.data.like_count}</div>
+                                </div>
+                                <div className="detail__description">
+                                    {this.props.location.state.data.comment}
+                                </div>
                             </div>
                         </div>
+                        <div className="button__container">
+                                <button onClick={this.handlePageGrid}>Grid</button>
+                                <button onClick={this.handlePageList}>List</button>
+                        </div>
                     </div>
-                    <div className="button__container">
-                        {content.map((section, index) =>  (
-                            <button onClick={() => changeItem(index)} key={index}>{section.tab}</button>
-                        ))}
-                    </div>
+                    {this.handlePageRender()}
                 </div>
-                {currentItem.content}
-            </div>
-            <div className="order"><Link to={link}>주문하기</Link></div>
-        </>
-    );
+                <div className="order"><Link to={this.link}>주문하기</Link></div>
+            </>
+        );
+    }
 }
 
 export default StoreView;
