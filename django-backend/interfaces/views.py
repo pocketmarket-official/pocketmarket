fa10dd6337ab8c4bb32df88f530aac04be450206
90majh <90majh@gmail.com>
2020-10-04 01:05:31 +0900

    you can get brands information from imt ASP correctly. insert with duplicate udpate

--- django-backend/interfaces/views.py
+++ django-backend/interfaces/views.py
@@ -1,26 +1,42 @@
-import os
-import sys
 import urllib.request
-# from rest_framework import viewsets
+import json
+from brands.models import Brand
 
-# client_id = "YOUR_CLIENT_ID"
-# client_secret = "YOUR_CLIENT_SECRET"
-# encText = urllib.parse.quote("검색할 단어")
 
 
 def InterfaceView(request):
     try:
+        ## compCd
         compCd = 'C0028'
+
+        ## brands
         url = "http://asp-test.imtsoft.me/api/pocketMarket/brandsBrand?compCd=" + compCd  # json 결과
-        # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
         request = urllib.request.Request(url)
-        # request.add_header("X-Naver-Client-Id", client_id)
-        # request.add_header("X-Naver-Client-Secret", client_secret)
         response = urllib.request.urlopen(request)
         rescode = response.getcode()
         if (rescode == 200):
-            response_body = response.read()
-            print(response_body.decode('euc-kr'))
+            response_body = response.read().decode('euc-kr')
+            response_body_json = json.loads(response_body)
+            for brand_imt in response_body_json:
+                brand_pktmkt, flag = Brand.objects.get_or_create(brandCd=brand_imt.get('BRAND_CD'),
+                                                          defaults={
+                                                              'brandName': brand_imt.get('BRAND_NM'),
+                                                              'useYn':brand_imt.get('USE_YN'),
+                                                              'insDt':brand_imt.get('INS_DT'),
+                                                              'insUs':brand_imt.get('INS_US'),
+                                                              'modDt':brand_imt.get('MOD_DT'),
+                                                              'modUs':brand_imt.get('MOD_US')
+                                                          })
+                if not flag:
+                    brand_pktmkt.brandName = brand_imt.get('BRAND_NM')
+                    brand_pktmkt.useYn = brand_imt.get('USE_YN')
+                    brand_pktmkt.insDt = brand_imt.get('INS_DT')
+                    brand_pktmkt.insUs = brand_imt.get('INS_US')
+                    brand_pktmkt.modDt = brand_imt.get('MOD_DT')
+                    brand_pktmkt.modUs = brand_imt.get('MOD_US')
+                    brand_pktmkt.save()
+
+        ##stores
         else:
             print("Error Code:" + rescode)
         return
