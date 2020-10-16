743a2f41b830e522be4117d0187f87e5616c104f
90majh <90majh@gmail.com>
2020-10-16 15:42:36 +0900

    modify ip

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0:8000', 'localhost', '127.0.0.1', '13.124.90.128']
+ALLOWED_HOSTS = ['0.0.0.0:8000', 'localhost', '127.0.0.1', '13.124.90.138']
 
 
 # Application definition
