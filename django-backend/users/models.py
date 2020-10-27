6822c03ad5c2df7784510a574638ace433e0c8ab
90majh <90majh@gmail.com>
2020-10-27 22:09:35 +0900

    make Point model

--- django-backend/users/models.py
+++ django-backend/users/models.py
@@ -98,3 +98,16 @@ class User(AbstractUser):
     testField = models.CharField(max_length=30, default='')
 
     pass
+
+class Point(models.Model):
+    """ Point Model Definition """
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, default=1)
+    remainPoint = models.IntegerField(null=True)
+    totExchangeLike = models.IntegerField(null=True)
+    totExchangePoint = models.IntegerField(null=True)
+    totSpendPoint = models.IntegerField(null=True)
+    lastExchangeDate = models.CharField(max_length=8, null=True)
+    insDt = models.DateTimeField(default=datetime.now(), null=True)
+    insUs = models.CharField(max_length=30, default='defaultValue', null=True)
+    modDt = models.DateTimeField(default=datetime.now(), null=True)
+    modUs = models.CharField(max_length=30, default='defaultValue', null=True)
