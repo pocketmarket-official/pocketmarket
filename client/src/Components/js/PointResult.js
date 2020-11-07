1096d10913145b24cd2ab20b2252476579697caf
ak74963 <ak74963@gmail.com>
2020-11-07 17:56:22 +0900

    PointHistory page design

--- client/src/Components/js/PointResult.js
+++ client/src/Components/js/PointResult.js
@@ -9,17 +9,34 @@ class PointResult extends React.Component {
                 return (
                         <>
                             <div className="result__date">{data.date}</div>
+                            <div className="result__detail2">
                             {data.point.map((item) => {
-                                return (
-                                    <>
-                                        <div className="result__detail">
-                                            <div className="result__conversion">전환</div>
-                                            <div className="result__likes">-{item} 좋아요</div>
-                                            <div className="result__point">+{item}PM</div>
-                                        </div>
-                                    </>
-                                );
+                                {if(item>=0){
+                                    return (
+                                        <>
+                                            <div className="result__detail">
+                                                <div className="result__conversion">전환</div>
+                                                <div className="result__likes">-{item} 좋아요</div>
+                                                <div className="result__point">+{item} PM</div>
+                                            </div>
+                                        </>
+                                    );
+                                }
+                                else{
+                                    return (
+                                        <>
+                                            <div className="result__detail">
+                                                <div className="result__use">사용</div>
+                                                <div className="result__likes">{item}</div>
+                                                <div className="result__point">-500 PM</div>
+                                            </div>
+                                        </>
+                                    );
+                                }
+
+                                };
                             })}
+                            </div>
                         </>
                 );
             })
