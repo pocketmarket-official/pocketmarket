f16224313ee5d172c233f37ff59de9ca1af1fd9d
Junhyeok Lee <hasu@imtsoft.org>
2020-11-12 17:37:31 +0900

    myplace

--- client/src/Components/js/myplaceJSX.js
+++ client/src/Components/js/myplaceJSX.js
@@ -2,27 +2,29 @@ import React from 'react';
 
 import '../scss/myPlaceJSX.scss';
 
+import rewrite from '../../assets/my_place_full/ico_rewrite.png';
+import trash from '../../assets/my_place_full/ico_trash.png';
 
 let temp = [
     {
         user_id: '노민철',
         seq: 1,
-        stor_addr1: '경기도 안양시 동안구',
-        stor_addr2: '어쩌구저쩌구',
+        stor_addr1: '서울특별시 종로구 간석동',
+        stor_addr2: '1124-12',
         title: '집',
     },
     {
         user_id: '노민철',
         seq: 2,
-        stor_addr1: '서울특별시 강남구 역삼동',
-        stor_addr2: '어쩌구저쩌구',
+        stor_addr1: '경기 여수시 장현동',
+        stor_addr2: '537-52',
         title: '회사',
     },
     {
         user_id: '노민철',
         seq: 3,
-        stor_addr1: '서울특별시 성북구 안암동',
-        stor_addr2: '어쩌구저쩌구',
+        stor_addr1: '충북 음성군 금왕읍 무곡로',
+        stor_addr2: '514-22',
         title: '학교',
     },
 ]
@@ -32,19 +34,21 @@ function MyplaceJSX() {
         temp.map((data) => {
             return (
                 <>
-                    <div className="myplace__container">
-                        <div className="myplace__content">
-                            <div className="myplace__name">{data.title}</div>
-                            <div className="myplace__address">{data.stor_addr1} {data.stor_addr2}</div>
-                            <div className="myplace__button">
-                                <div className="myplace__write" onClick={() => {
+
+                        <tr className="myplace__content">
+                            <td className="myplace__name">
+                                <div>{data.title}</div>
+                            </td>
+                            <td className="myplace__address">{data.stor_addr1} {data.stor_addr2}</td>
+                            <td colSpan="2" className="myplace__button">
+                                <td className="myplace__write" onClick={() => {
                                     const elt = document.getElementById("myplace__modal");
                                     elt.classList.remove("hidden")
-                                }}>Write</div>
-                                <div className="myplace__delete">X</div>
-                            </div>
-                        </div>
-                    </div>
+                                }}><img src={rewrite}/></td>
+                                <td className="myplace__delete"><img src={trash}/></td>
+                            </td>
+                        </tr>
+
                 </>
             );
         })
--- client/src/Routes/Myplace.js
+++ client/src/Routes/Myplace.js
@@ -4,7 +4,9 @@ import HeaderBack from '../Components/js/HeaderBack';
 import MyplaceJSX from '../Components/js/myplaceJSX';
 import '../Components/scss/Myplace.scss';
 import rewrite from '../assets/my_place_full/img_name.png';
-import close from "../assets/order_status_pop/btn_close.png";
+import close from "../assets/my_place_full/btn_close.png";
+import ico_close from "../assets/my_place_full/ico_close.png";
+import search from "../assets/my_place_full/ico_search.png";
 
 function Myplace() {
     return (
@@ -31,9 +33,69 @@ function Myplace() {
             <HeaderBack url='/mypage' />
             <div className="myplace">
                 <div className="myplace__favorite">
-                    <div className="myplace__title">My Place</div>
-                    <MyplaceJSX />
-                    <Link to="/mypage/myplace/search"><input type="button" value="위치 검색하기" /></Link>
+                    <div className="myplace__title">
+                        <text>My Place</text>
+                    </div>
+                    <table className="myplace__savePlace">
+                        <th colSpan="4" className="myplace__tableHeader">
+                            <td>저장 장소</td>
+                        </th>
+                        <MyplaceJSX />
+                    </table>
+
+                    <table className="myplace__addressSave">
+                        <th>
+                            <td>주소 저장</td>
+                        </th>
+                        <tr className="myplace__content">
+                            <td className="myplace__message">지번, 도로명, 건물명을 입력하세요</td>
+                        </tr>
+                        <tr className="myplace__content">
+                            <td className="myplace__addrSearch">
+                                <form>
+                                    <input type="text" placeholder="예) 배민동 12-3 또는 배민아파트"></input>
+                                    <Link to="/mypage/myplace/search">
+                                        <button type="submit"><img src={search}/></button>
+                                    </Link>
+                                </form>
+                            </td>
+                        </tr>
+                        <tr className="myplace__content">
+                            <td className="myplace__button">
+                                <button>현 위치로 주소 설정</button>
+                            </td>
+                        </tr>
+                    </table>
+
+                    <table className="myplace__recentAddress">
+                        <th colSpan="3" className="myplace__tableHeader">
+                            <td>최근 주소</td>
+                        </th>
+                        <tr className="myplace__content">
+                            <td colSpan="2" className="myplace__address">안양시 동안구 호계동 1027 안양 아이티밸리 801호 아이엠티소프트</td>
+                            <td rowSpan="2" className="myplace__button">
+                                <td className="myplace__delete"><img src={ico_close}/></td>
+                            </td>
+                        </tr>
+                        <tr className="myplace__content2">
+                            <td className="myplace__newZipCdNm">
+                                <div>도로명</div>
+                            </td>
+                            <td className="myplace__newZipCd">엘에스로91번길 16-39 안양it밸리 801호.아이엠티소프트</td>
+                        </tr>
+                        <tr className="myplace__content">
+                            <td colSpan="2" className="myplace__address">서울시 강동구 천호동 154-2 13521호</td>
+                            <td rowSpan="2" className="myplace__button">
+                                <td className="myplace__delete"><img src={ico_close}/></td>
+                            </td>
+                        </tr>
+                        <tr className="myplace__content2">
+                            <td className="myplace__newZipCdNm">
+                                <div>도로명</div>
+                            </td>
+                            <td className="myplace__newZipCd">성안로 173번길 서울시 13521호</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </>
