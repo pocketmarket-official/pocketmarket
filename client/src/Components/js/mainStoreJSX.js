7f38ad9a555d8870e1b6693bd3242ea796132813
Peter-Roh <rmc2@naver.com>
2020-11-18 23:37:28 +0900

    like btn

--- client/src/Components/js/mainStoreJSX.js
+++ client/src/Components/js/mainStoreJSX.js
@@ -19,6 +19,7 @@ class StoreJSX extends React.Component {
             like: 0,
             storeId: 0,
             userId: 0,
+            likeYn: '',
         }
     }
 
@@ -42,10 +43,46 @@ class StoreJSX extends React.Component {
             axios.post('http://localhost:8000/storeLike/', transData)
             .then((res) => {
                 let like = res.data.likeCnt;
+                let likeYn = res.data.likeYn;
                 this.setState({
                     like: like,
                     storeId: data.id,
                     userId: user.id,
+                    likeYn: likeYn,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        });
+    }
+
+    componentDidUpdate() {
+        let data = this.props.data;
+        let cookie_token = cookie.load("access_token");
+        let user_email = storage.get(cookie_token);
+
+        axios.get("/api/users_user/")
+        .then((res) => {
+            let user = res.data.find((elt) => {
+                if(elt.email === user_email) {
+                    return true;
+                }
+            });
+            
+            let transData = {
+              "storeId":data.id, "userId":user.id
+            };
+            
+            axios.post('http://localhost:8000/storeLike/', transData)
+            .then((res) => {
+                let like = res.data.likeCnt;
+                let likeYn = res.data.likeYn;
+                this.setState({
+                    like: like,
+                    storeId: data.id,
+                    userId: user.id,
+                    likeYn: likeYn,
                 });
             })
             .catch((err) => {
@@ -73,10 +110,16 @@ class StoreJSX extends React.Component {
                                 <div className="tags__tag">@반포 낭만달빛마켓</div>
                                 <button className="tags__likes" onClick={(e) => {
                                     e.preventDefault();
+                                    let likeYn;
+                                    if(this.state.likeYn === 'Y') {
+                                        likeYn = 'N';
+                                    } else if(this.state.likeYn === 'N') {
+                                        likeYn = 'Y';
+                                    }
                                     axios.post("http://localhost:8000/api/stores_storeLike/", {
                                         "store": this.state.storeId,
                                         "user": this.state.userId,
-                                        "likeYn": 'y',
+                                        "likeYn": likeYn,
                                     })
                                     .then(() => {
                                     {/* 좋아요 개수 가져오기 */}
@@ -85,6 +128,7 @@ class StoreJSX extends React.Component {
                                             "userId": this.state.userId,
                                         })
                                         .then((res) => {
+                                            console.log(res);
                                             let like = res.data.likeCnt;
                                             this.setState({
                                                 like: like,
