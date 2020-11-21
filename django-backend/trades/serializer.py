e3b7e1ca67e6baeaad59cb984e6a1b9de7aca4ef
90majh <90majh@gmail.com>
2020-11-21 20:25:32 +0900

    modify again

--- django-backend/trades/serializer.py
+++ django-backend/trades/serializer.py
@@ -67,7 +67,6 @@ class SaleDetailSerializer(serializers.ModelSerializer):
             'supAmt',
             'taxYn',
             'offTaxAmt',
-            'taxFlag',
             'totDcAmt',
             'pointDcAmt',
             'saleTime',
