1e0d612277af0e1e586fc6da016c1a58493bbfb8
90majh <90majh@gmail.com>
2020-11-23 00:56:34 +0900

    hide mypage lists

--- client/src/Routes/Mypage.js
+++ client/src/Routes/Mypage.js
@@ -11,10 +11,10 @@ function Mypage() {
             <div className="mypage">
                 <div className="mypage__box">
                     <div className="mypage__list"><Link to="/mypage/collections">포켓마켓도감</Link><p>{">"}</p></div>
-                    <div className="mypage__list"><Link to="/mypage/myplace">주소관리</Link><p>{">"}</p></div>
-                    <div className="mypage__list"><Link to="/mypage/order">주문 / 결제 이력</Link><p>{">"}</p></div>
+                    {/*<div className="mypage__list"><Link to="/mypage/myplace">주소관리</Link><p>{">"}</p></div>*/}
+                    {/*<div className="mypage__list"><Link to="/mypage/order">주문 / 결제 이력</Link><p>{">"}</p></div>*/}
                     {/*<div className="mypage__list"><Link to="/mypage/point">포인트 / 좋아요 이력</Link><p>{">"}</p></div>*/}
-                    <div className="mypage__list"><Link to="/review">리뷰 목록</Link><p>{">"}</p></div>
+                    {/*<div className="mypage__list"><Link to="/review">리뷰 목록</Link><p>{">"}</p></div>*/}
                     <div className="mypage__list"><Link to="/mypage/questions">문의 내역</Link><p>{">"}</p></div>
                 </div>
                 <div className="setting__box">
