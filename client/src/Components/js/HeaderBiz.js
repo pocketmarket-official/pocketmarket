05ec87fd299ac7b0d6b6bcabf421ad98f2cfeb7c
90majh <90majh@gmail.com>
2020-11-23 01:59:16 +0900

    modify my to alarm

--- client/src/Components/js/HeaderBiz.js
+++ client/src/Components/js/HeaderBiz.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from 'react-router-dom';
 import btnBack from "../../assets/common/btn_bs.png";
 import bi from "../../assets/common/BI.png";
 import btnMy from "../../assets/common/btn_my.png";
+import btnAlarm from "../../assets/common/btn_alarm.png";
 
 function HeaderBiz() {
     const history = useHistory();
@@ -11,7 +12,8 @@ function HeaderBiz() {
         <div className="header__box">
             <img className="header__back"  onClick={() => {history.goBack()}} src={btnBack}/>
             <Link to="/main"><img className="header__bi" src={bi} alt="logo" /></Link>
-            <Link to="/biz/mypage"><img className="header__my" src={btnMy} alt="my" /></Link>
+            {/*<Link to="/biz/mypage"><img className="header__my" src={btnMy} alt="my" /></Link>*/}
+            <img className="header__alarm" src={btnAlarm} alt="alarm" />
         </div>
     );
 }
