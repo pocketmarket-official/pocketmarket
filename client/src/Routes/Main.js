28098de14d34bc55148050c151e965d45cc43af2
90majh <90majh@gmail.com>
2020-11-22 17:02:06 +0900

    revert

--- client/src/Routes/Main.js
+++ client/src/Routes/Main.js
@@ -178,9 +178,6 @@ class Main extends React.Component {
         return (
             <>
                 <Header />
-                asdfdsf
-                {this.state.userId}
-                asdfadsf
                 <div className="main">
                     <div className="main__btns">
                         <div className="btn__left">
