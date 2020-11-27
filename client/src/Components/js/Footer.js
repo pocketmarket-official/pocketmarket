41ee15594c0d725f147d39a7e82c9c114d189940
Peter-Roh <rmc2@naver.com>
2020-11-27 18:24:40 +0900

    change footer

--- client/src/Components/js/Footer.js
+++ client/src/Components/js/Footer.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+
+const content = [
+    {
+        tab: "상호명",
+        content: "포켓마켓",
+    },
+    {
+        tab: "사업자번호",
+        content: "165 11 01485",
+    },
+    {
+        tab: "대표",
+        content: "장문진",
+    },
+    {
+        tab: "연락처",
+        content: "070-4629-3908",
+    },
+    {
+        tab: "주소",
+        content: "서울특별시 서초구 한강대로 369, 12층 89호",
+    },
+];
+
+const useTabs = (initialTab, allTabs) => {
+    const [currentIndex, setCurrentIndex] = useState(initialTab);
+
+    if(!allTabs || !Array.isArray(allTabs)) {
+        return;
+    }
+
+    return {
+        currentItem: allTabs[currentIndex], 
+        changeItem: setCurrentIndex
+    };
+};
 
 function Footer() {
+    const { currentItem, changeItem } = useTabs(0, content);
+
     return (
         <>
             <footer>
                 <div className="footer__top">
-                    <div className="footer__column">
-                        <div>상호명</div>
-                        <div>포켓마켓</div>
-                    </div>
-                    <div className="footer__column">
-                        <div>사업자번호</div>
-                        <div>165 11 01485</div>
-                    </div>
-                    <div className="footer__column">
-                        <div>대표</div>
-                        <div>장문진</div>
-                    </div>
-                    <div className="footer__column">
-                        <div>연락처</div>
-                        <div>010-9410-9188</div>
-                    </div>
+                    {
+                        content.map((section, index) => (
+                            <div onClick={() => changeItem(index)}>
+                                <div>{section.tab}</div>
+                            </div>
+                        ))
+                    }
                 </div>
+                <div className="footer__column">{currentItem.content}</div>
                 <div className="footer__middle">
                     포켓마켓은 통신판매중개자이며 통신판매의 당사자가 아닙니다. 따라서 포켓마켓은 상품, 거래정보 및 거래에 책임을 지지 않습니다. 
                 </div>
