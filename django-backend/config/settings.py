661e76ffca6a6480ed73281fc885c73e36baac04
Peter-Roh <rmc2@naver.com>
2020-12-10 18:12:48 +0900

    configuring things for deployment

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -220,7 +220,7 @@ elif STATE == "dev":
     DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
     STATICFILES_STORAGE = 'config.storage_backends.StaticStorage'
 
-    STATIC_URL = 'https://%s.%s/static/' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_HOST)
+#    STATIC_URL = 'https://%s.%s/static/' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_HOST)
 
     MEDIA_URL = 'https://%s.%s/media/' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_HOST)
 
