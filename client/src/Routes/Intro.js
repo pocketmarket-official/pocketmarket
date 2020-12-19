5694d140209f5ae733e2cb73a586a95f9966dcab
90majh <90majh@gmail.com>
2020-12-19 15:21:10 +0900

    tmp

--- client/src/Routes/Intro.js
+++ client/src/Routes/Intro.js
@@ -8,9 +8,11 @@ import storage from "../storage";
 import axios from "axios";
 
 function tmp(){
-    axios.post("http://13.124.90.138:8000/api/trades_test/",{
-        char:'test2'
-    });
+    console.log('tmp');
+    alert('tmp');
+    // axios.post("http://13.124.90.138:8000/api/trades_test/",{
+    //     char:'test2'
+    // });
 }
 
 function makeTokenSaveScript(token) {
