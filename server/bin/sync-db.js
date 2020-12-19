a59a336bde859fa1b442c3cd270b1d8b865c453f
Peter-Roh <rmc2@naver.com>
2020-12-19 18:48:07 +0900

    deploy

--- server/bin/sync-db.js
+++
@@ -1,9 +0,0 @@
-const models = require('../models');
-
-module.exports = () => {
-    const options = {
-        force: process.env.NODE_ENV === 'test' ? true : false
-    }
-    console.log(process.env.NODE_ENV + ' mode');
-    return models.sequelize.sync(options);
-}
--- server/routes/festival.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-import controller from '../controller/festival.controller';
-
-/* GET users listing. */
-router.get('/', controller.index);
-router.post('/', controller.create);
-
-module.exports = router;
