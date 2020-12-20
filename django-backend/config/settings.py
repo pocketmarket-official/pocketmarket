3bb5db72e79b0ab8662a807ffc3f280cbbde9008
90majh <90majh@gmail.com>
2020-12-21 02:22:57 +0900

    tmp

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -23,14 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:8000',
-    'http://Pocketmarket-dev.eba-qcrhvmux.ap-northeast-2.elasticbeanstalk.com:3000',
-    'http://Pocketmarket-dev.eba-qcrhvmux.ap-northeast-2.elasticbeanstalk.com:8000',
-    'http://13.124.90.138:3000/',
-    'http://13.124.90.138:8000/',
+    'http://13.124.90.138:3000',
+    'http://13.124.90.138:8000',
 ]
 
 # Application definition
@@ -196,8 +190,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://Pocketmarket-dev.eba-qcrhvmux.ap-northeast-2.elasticbeanstalk.com:8000',
     'http://13.124.90.138:3000',
     'http://13.124.90.138:8000',
-    'http://localhost:8000',
-    'http://localhost:8000',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
