517d95f93e2cf0d756c3531cdc193b1ee2a9f262
90majh <90majh@gmail.com>
2020-12-29 01:49:39 +0900

    use_tz true for dev. because of mysql

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -213,6 +213,8 @@ elif STATE == "dev":
             },
         }
     }
+
+    USE_TZ = True
     # AWS and S3 and
     # AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     # AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
