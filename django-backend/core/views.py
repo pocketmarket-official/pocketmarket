fd6408c25e4e3e43102527bd2c327126bdeccfa4
90majh <90majh@gmail.com>
2020-12-29 16:14:47 +0900

    delete jh state from trade module

--- django-backend/core/views.py
+++ django-backend/core/views.py
@@ -194,10 +194,6 @@ def trade(request):
         elif STATE == 'production':
             domain = 'https://asp.imtsoft.me/api/'
             compCd = 'C0023'
-        elif STATE == 'jh':
-            domain = 'https://asp.imtsoft.me/api/'
-            compCd = 'C0023'
-
 
         # constant
 
