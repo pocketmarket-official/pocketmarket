93b1fa66a844050558f268f4030b31e912140b3e
90majh <90majh@gmail.com>
2020-12-30 13:57:06 +0900

    add jh state

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -264,6 +264,40 @@ elif STATE == "production":
     MEDIA_URL = 'https://%s.%s/media/' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_HOST)
 
 # -----------------------------------------------------------------------------------------
+elif STATE == "jh":
+    DEBUG = False
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get("DB_JH_NAME"),
+            'USER': os.environ.get("DB_JH_USER"),
+            'PASSWORD': os.environ.get("DB_JH_PASSWORD"),
+            'PORT': os.environ.get("DB_JH_PORT"),
+            'HOST': os.environ.get("DB_JH_HOST"),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                # 'charset': 'utf8mb4',
+            },
+        }
+    }
+
+    USE_TZ = True
+    AWS and S3 and
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = 'pocketmarket-dev'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_HOST = 's3.ap-northeast-2.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_QUERYSTRING_AUTH = False
+
+    DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
+    STATICFILES_STORAGE = 'config.storage_backends.StaticStorage'
+    STATIC_URL = 'https://%s.%s/static/' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_HOST)
+    MEDIA_URL = 'https://%s.%s/media/' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_HOST)
 
 
 elif STATE == "server:appDeploy":
