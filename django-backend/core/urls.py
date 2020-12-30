4902276c40e5255c87cb2f4273ad8f893dd2caad
90majh <90majh@gmail.com>
2020-12-30 12:57:26 +0900

    make trade resend url

--- django-backend/core/urls.py
+++ django-backend/core/urls.py
@@ -5,6 +5,7 @@ from core import views
 urlpatterns = [
     path("login/kakao/callback/", views.kakao_callback, name="kakao-callback"),
     path("trade/", views.trade),
+    path("tradeReSend/", views.tradeReSend),
     path("saveToken/", views.saveToken),
     path("saveTokenStore/", views.saveTokenStore),
     path("pushSend_makeComplete/", views.pushSend_makeComplete),
