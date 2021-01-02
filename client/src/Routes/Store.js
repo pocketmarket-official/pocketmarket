689f4f44624fdf43476cc82ac36de78e42ab5256
90majh <90majh@gmail.com>
2021-01-02 15:40:26 +0900

    controll render div up to the class of additional imgs

--- client/src/Routes/Store.js
+++ client/src/Routes/Store.js
@@ -58,7 +58,6 @@ class StoreView extends React.Component {
 
     handlePageRender() {
         if(this.state.current === 0) {
-            console.log(this.state.reviews)
             return (
             <>
                 <div className="store__review__grid" id="review__container">
@@ -74,7 +73,7 @@ class StoreView extends React.Component {
                                                     state: {review: review},
                                                 }}>
                                                     {
-                                                        (review.img2 === null || review.img2 === '') ?
+                                                        (review.img2 === null || review.img2 === '' || review.img2 === undefined) ?
                                                             <div><img src={review.img1} alt="review" /></div> //추가이미지 없는애
                                                             :
                                                             <div className="photo"><img src={review.img1} alt="reviews" /></div> //추가이미지 있는애
