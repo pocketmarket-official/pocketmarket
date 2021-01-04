c4164c78cfaf8191e868a0682fe3206e0ec6b2f7
90majh <90majh@gmail.com>
2021-01-04 18:07:52 +0900

    move bootpay test module to intro page

--- client/src/Routes/OrderInfo.js
+++ client/src/Routes/OrderInfo.js
@@ -5,32 +5,6 @@ import HeaderBiz from "../Components/js/HeaderBiz";
 import axios from "axios";
 import {cookieCheck_rejectGuest} from "../Components/js/CookieCheck.js"
 
-const applicationId = process.env.REACT_APPLE_APP_ID;
-
-window.registerAppId = (function () {
-    console.log("==1==");
-    console.log(applicationId);
-    // window.BootPay.setApplicationId(applicationId);
-});
-
-window.registerAppIdDemo = (function () {
-    console.log("==2==");
-    console.log(applicationId);
-    // window.setApplicationId(applicationId);
-});
-
-window.setDevice = (function () {
-    console.log("==3==");
-    console.log(applicationId);
-    // window.BootPay.setDevice('IOS');
-});
-
-window.startTrace = (function () {
-    console.log("==4==");
-    console.log(applicationId);
-    // window.BootPay.startTrace();
-});
-
 
 
 class OrderInfo extends React.Component {
