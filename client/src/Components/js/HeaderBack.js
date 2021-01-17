a7f8009f43e8c7817eb16a28557d2eab85cba5ca
90majh <90majh@gmail.com>
2021-01-17 22:30:56 +0900

    clear cache 1. clear cache when click bi

--- client/src/Components/js/HeaderBack.js
+++ client/src/Components/js/HeaderBack.js
@@ -4,6 +4,7 @@ import { Link, useHistory } from 'react-router-dom';
 import btnBack from '../../assets/common/btn_bs.png';
 import btnAlarm from '../../assets/common/btn_alarm.png';
 import bi from '../../assets/common/BI.png';
+import { clearCache } from 'react-router-cache-route'
 
 
 function HeaderBack(props) {
@@ -11,8 +12,8 @@ function HeaderBack(props) {
 
     return (
             <div className="header__box">
-                <img className="header__back"  onClick={() => {history.goBack()}} src={btnBack}/>
-                <Link to="/index"><img className="header__bi" src={bi} alt="logo" /></Link>
+                <img className="header__back"  onClick={() => {history.goBack();}} src={btnBack}/>
+                <Link to="/index" onClick={()=>clearCache()}><img className="header__bi" src={bi} alt="logo" /></Link>
                 <div className="fake"></div>
                 {/*<img className="header__alarm" src={btnAlarm} alt="alarm" />*/}
             </div>
