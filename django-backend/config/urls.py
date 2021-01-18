f671a70bd5d3f28b073173c31bb4973a066e0fe0
Peter-Roh <rmc2@naver.com>
2021-01-18 15:24:21 +0900

    fixed error in django - react connection

--- django-backend/config/urls.py
+++ django-backend/config/urls.py
@@ -41,6 +41,7 @@ from django.urls import re_path
 from django.views.generic import TemplateView
 
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 
 
@@ -111,4 +112,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-#urlpatterns += [re_path(r'^(?:.*)?$', TemplateView.as_view(template_name="index.html"), name="index")]
+urlpatterns += [url(r'^', TemplateView.as_view(template_name='index.html'))]
