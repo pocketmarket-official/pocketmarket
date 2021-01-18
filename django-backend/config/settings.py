4a14b5bb8bab80441ac5c58408d4a58930a84466
90majh <90majh@gmail.com>
2021-01-18 23:02:34 +0900

    modify staticfiles_dirs from build/static to build

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "build/static/"),
+    os.path.join(BASE_DIR, "build/"),
 ]
 
 MEDIA_URL = '/uploads/'
